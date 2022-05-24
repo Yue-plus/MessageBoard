a188d1de3aff7548aa04e49ce7c490660eccb546
Yue-plus <Yue_plus@foxmail.com>
2022-05-24 17:15:44 +0800

    done fetch 实现

---
+++ src/main/resources/static/js/fetch.js
@@ -0,0 +1,30 @@
+function putMessage() {
+  fetch("/putMessage", {
+    method: "PUT",
+    mode: "cors",
+    body: new FormData(document.getElementById("PutMessage"))
+  })
+      .then(response => response.text())
+      .then(data => console.log(data));
+}
+
+function updateMessageBoard() {
+  fetch("/getMessages")
+      .then(response => response.json())
+      .then(data => {
+        let temp = "";
+
+        for (let i in data) {
+          temp += `${data[i].id} 楼的 ${data[i].nickName} 说：${data[i].message}<br />`;
+        }
+
+        document.getElementById("MessageBoard").innerHTML = temp;
+      });
+}
+
+function autoUpdateMessageBoard() {
+  updateMessageBoard();
+  setTimeout(autoUpdateMessageBoard, 2000);
+}
+
+autoUpdateMessageBoard()
